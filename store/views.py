2a93a7d2de2c49ae9b164b04bc201f686ff845ed
Achinga <56168272+AchingaChris@users.noreply.github.com>
2020-05-26 18:23:58 +0300

    query totals for cart and checkout

--- store/views.py
+++ store/views.py
@@ -13,11 +13,24 @@ def cart(request):
     # see if user is authenticated
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create()
-
-    context = {}
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for non-logged in user
+        items = []
+        order = {'order.get_cart_items':0, 'order.get_cart_total':0}
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    # see if user is authenticated
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for non-logged in user
+        items = []
+        order = {'order.get_cart_items':0, 'order.get_cart_total':0}
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
