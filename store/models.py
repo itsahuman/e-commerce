96d3523291f65f757f04e2bedf9a24948594f872
Achinga <56168272+AchingaChris@users.noreply.github.com>
2020-05-26 18:23:25 +0300

    added property decorators forcart totals

--- store/models.py
+++ store/models.py
@@ -37,11 +37,32 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	# get total amount of the order
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total
+
+
+	# get total amount items in cart
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
+
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	# getting total price for each item in the cart
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
 
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
